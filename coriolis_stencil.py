4a82a8fbba713db5096ecb6f2f2ddfb49329da25
Eddie Davis <eddied@vulcan.com>
2020-02-21 10:15:08 -0800

    Fix coriolis_stencil, gt4py errors on accum ops

--- coriolis_stencil.py
+++ coriolis_stencil.py
@@ -15,7 +15,7 @@ def coriolis_stencil(
     with computation(FORWARD), interval(...):
         z_fv_north = fc * (v_nnow + v_nnow[1, 0, 0])
         z_fv_south = fc[0, -1, 0] * (v_nnow[0, -1, 0] + v_nnow[1, -1, 0])
-        u_tens += 0.25 * (z_fv_north + z_fv_south)
+        u_tens = u_tens + (0.25 * (z_fv_north + z_fv_south))
         z_fu_east = fc * (u_nnow + u_nnow[0, 1, 0])
         z_fu_west = fc[-1, 0, 0] * (u_nnow[-1, 0, 0] + u_nnow[-1, 1, 0])
-        v_tens -= 0.25 * (z_fu_east + z_fu_west)
+        v_tens = v_tens - (0.25 * (z_fu_east + z_fu_west))

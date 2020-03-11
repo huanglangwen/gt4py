1e03d6eda606a6a148e1f42175e018f603250c6e
Enrique G. Paredes <enriqueg@cscs.ch>
2020-03-11 12:49:13 +0100

    Fix some missing GPU tests

--- tests/test_integration/test_code_generation.py
+++ tests/test_integration/test_code_generation.py
@@ -51,6 +51,29 @@ def test_generation_cpu(name, backend):
     stencil(**args, origin=(10, 10, 10), domain=(3, 3, 3))
 
 
+@pytest.mark.requires_gpu
+@pytest.mark.parametrize(
+    ["name", "backend"], itertools.product(stencil_definitions.names, GPU_BACKENDS)
+)
+def test_generation_gpu(name, backend):
+    stencil_definition = stencil_definitions[name]
+    externals = externals_registry[name]
+    stencil = gtscript.stencil(backend, stencil_definition, externals=externals)
+    args = {}
+    for k, v in stencil_definition.__annotations__.items():
+        if isinstance(v, gtscript._FieldDescriptor):
+            args[k] = gt_storage.ones(
+                dtype=v.dtype,
+                mask=gtscript.mask_from_axes(v.axes),
+                backend=backend,
+                shape=(23, 23, 23),
+                default_origin=(10, 10, 10),
+            )
+        else:
+            args[k] = v(1.5)
+    stencil(**args, origin=(10, 10, 10), domain=(3, 3, 3))
+
+
 def test_temporary_field_declared_in_if_raises():
 
     from gt4py.frontend.gtscript_frontend import GTScriptSymbolError

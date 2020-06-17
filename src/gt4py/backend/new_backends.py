c277f3c24a2c7b67d93ee3c9e1bf2f663c2322e7
Eddie Davis <eddied@vulcan.com>
2020-06-17 08:13:33 -0700

    Add 'debug' flag to add printf statements

--- src/gt4py/backend/new_backends.py
+++ src/gt4py/backend/new_backends.py
@@ -254,6 +254,7 @@ class OptExtGenerator(gt_backend.GTPyExtGenerator):
             max_threads=max_threads,
             extra_threads=extra_threads,
             do_k_parallel=False,
+            debug=True,
         )
 
         sources = {}

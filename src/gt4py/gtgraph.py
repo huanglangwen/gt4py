04ffc65e5f6abd80b7276bcaefdb6b4204074caa
Langwen Huang <huanglangwen@outlook.com>
2021-05-05 15:50:05 +0000

    Finish testing async and fix minor bugs, delete old gtgraph code

--- src/gt4py/gtgraph.py
+++ src/gt4py/gtgraph.py
@@ -157,7 +157,8 @@ def async_invoke(async_context: AsyncContext, stencil: StencilObject, *args, **k
         streams = stream_pool * num_kernels
     else:
         streams = stream_pool
-    stencil(*args, async_launch=True, streams=streams, **kwargs)
+    stream_ptrs = [stream.ptr for stream in streams]
+    stencil(*args, async_launch=True, streams=stream_ptrs, **kwargs)
     # insert events
     done_events = [cupy.cuda.Event(block=False, disable_timing=True) for _ in range(num_streams)]
     for i in range(1, num_streams):
@@ -176,6 +177,9 @@ class InsertAsync(ast.NodeTransformer):
         return astor.to_source(maker.ast_root, add_line_information=True)
 
     def __init__(self, definition, ctx, num_streams_init):
+        # check AsyncContext and async_invoke is in ctx as well
+        #if ("AsyncContext" not in ctx) and ("async_invoke" not in ctx):
+        #    raise ValueError("Please import `AsyncContext` and `async_invoke` first")
         self.ast_root = astor.code_to_ast(definition)
         self.stencil_ctx = {k: ctx[k] for k in ctx if isinstance(ctx[k], StencilObject) and
                                 hasattr(ctx[k], "pyext_module")}
@@ -193,6 +197,9 @@ class InsertAsync(ast.NodeTransformer):
 
     def insert_init(self, node: ast.FunctionDef):
         # num_streams_guess = min(20, sum(eval(stencil).pyext_module.num_kernels() for stencil in self.stencil_ctx))
+        import_node = ast.ImportFrom(module='gt4py.gtgraph', names=[ast.alias(name='AsyncContext', asname=None),
+                                                                    ast.alias(name='async_invoke', asname=None)],
+                                     level=0, lineno=node.body[0].lineno)
         call_node = ast.Call(func=ast.Name(id="AsyncContext", ctx=ast.Load()),
                              args=[ast.Constant(value=self.num_streams_init, kind=None)],
                              keywords=[])
@@ -203,92 +210,8 @@ class InsertAsync(ast.NodeTransformer):
                                 attr='wait_finish', ctx=ast.Load()), args=[], keywords=[]),
                             lineno=node.body[-1].lineno)
         new_node = ast.copy_location(ast.FunctionDef(name=node.name, args=node.args,
-                                                     body=[start_node]+node.body+[end_node],
+                                                     body=[import_node, start_node]+node.body+[end_node],
                                                      decorator_list=node.decorator_list,
                                                      returns=node.returns,
                                                      type_comment=node.type_comment), node)
         return new_node
-
-
-
-class GraphMaker(ast.NodeVisitor):
-    @classmethod
-    def apply(cls, definition, stencil_ctx: Dict[str, StencilObject]):
-        maker = cls(definition, stencil_ctx)
-        maker(maker.ast_root)
-        return maker.graph, maker.meta_data
-
-    def __init__(self, definition, stencil_ctx: Dict[str, StencilObject]):
-        assert isinstance(definition, types.FunctionType)
-        self.definition = definition
-        self.filename = inspect.getfile(definition)
-        self.source = inspect.getsource(definition)
-        self.ast_root = ast.parse(self.source)
-        self.graph = nx.DiGraph()
-        self.meta_data: Dict[str, Any] = {}
-        self.stencil_ctx: Dict[str, StencilObject] = stencil_ctx
-
-    def __call__(self, func_node: ast.FunctionDef):
-        self.visit(func_node)
-
-    def __contains__(self, node_name: str) -> bool:
-        return node_name in self.meta_data
-
-    def _add_node(self, node_name: str, node_meta: Any) -> None:
-        self.graph.add_node(node_name)
-        self.meta_data[node_name] = dict(node=node_meta)
-
-    def visit(self, node, **kwargs):
-        """Visit a node."""
-        method = "visit_" + node.__class__.__name__
-        visitor = getattr(self, method, self.generic_visit)
-        return visitor(node, **kwargs)
-
-    def visit_FunctionDef(self, node: ast.FunctionDef, **kwargs):
-        for arg in node.args.args:
-            self.visit(arg)
-        for entry in node.body:
-            self.visit(entry)
-
-    def visit_arg(self, node: ast.arg, **kwargs):
-        arg_name = node.arg
-        if isinstance(node.annotation, ast.Subscript):
-            container_type = node.annotation.value.id.lower()
-            if container_type == "dict":
-                arg_type = node.annotation.slice.value.elts[1].value
-        else:
-            arg_type = node.annotation.id
-        if "Field" in arg_type:
-            self._add_node(arg_name, node)
-
-    def visit_Assign(self, node: ast.Assign, **kwargs):
-        if isinstance(node.value, ast.Call):
-            func = node.value.func
-            func_name = func.attr if isinstance(func, ast.Attribute) else func.id
-            # TODO: This is very fv3core-specific, need to generalize for gt4py...
-            if func_name == "copy" or func_name.startswith("make_storage"):
-                for target in node.targets:
-                    self._add_node(target.id, node)
-
-    def visit_Call(self, node: ast.Call, **kwargs):
-        func = node.func
-        is_attr = isinstance(func, ast.Attribute)
-        func_name = func.attr if is_attr else func.id
-        if "Exception" not in func_name:
-            for arg in node.args:
-                if isinstance(arg, ast.Name):
-                    arg_name = arg.id
-                    if arg_name in self:
-                        if func_name not in self:
-                            self._add_node(func_name, node)
-                        # TODO: How to determine if in/out?
-                        self.graph.add_edge(arg.id, func_name)
-
-            for keyword in node.keywords:
-                arg_name = keyword.arg
-                if arg_name in self:
-                    if func_name not in self:
-                        self._add_node(func_name, node)
-                    self.graph.add_edge(arg.id, func_name)
-                elif arg_name == "domain" or arg_name == "origin":
-                    self.meta_data[func_name][arg_name] = keyword.value

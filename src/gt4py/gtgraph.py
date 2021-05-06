fd509ad20ba41b1a44d8b4c3da16acf453567d81
Langwen Huang <huanglangwen@outlook.com>
2021-05-06 18:54:03 +0200

    Add visualization for dependency graph

--- src/gt4py/gtgraph.py
+++ src/gt4py/gtgraph.py
@@ -26,13 +26,15 @@ import cupy.cuda
 import networkx as nx
 import types
 import astor
-from typing import Any, Callable, Dict, Tuple, List, Deque
+from typing import Any, Callable, Dict, Tuple, List, Deque, Optional
 from gt4py.stencil_object import StencilObject
 from gt4py import AccessKind
 from gt4py.storage import Storage
 from collections import deque
 from dataclasses import dataclass
 from time import sleep
+from graphviz import Digraph
+from uuid import uuid4
 
 _graphs: Dict[str, Tuple[Any, Dict[str, Any]]] = {}
 
@@ -59,16 +61,72 @@ class InvokedStencil():
     stencil: StencilObject
     access_info: Dict[str, AccessKind]
     done_event: cupy.cuda.Event
+    id: int
 
 class AsyncContext():
-    def __init__(self, num_streams, max_invoked_stencils = 50, blocking = False, sleep_time = 0.5):
-        self.stream_pool: List[cupy.cuda.Stream] = []
+    def __init__(self, num_streams, max_invoked_stencils = 50, blocking = False, sleep_time = 0.5, graph_record = False, name = None):
+        self.stream_pool: List[Optional[cupy.cuda.Stream]] = []
         self.add_streams(num_streams)
         self.invoked_stencils: Deque[InvokedStencil] = deque()
         self.max_invoked_stencils: int = max_invoked_stencils
         self.known_num_fields: int = 0
         self.blocking: bool = blocking # synchronize all stencils, debug only
-        self.sleep_time: float = 0.5 # (second) longer sleep saves cpu cycle but lower resolution in timing
+        self.sleep_time: float = sleep_time # (second) longer sleep saves cpu cycle but lower resolution in timing
+        self._graph_record: bool = False
+        self._stencil_id_ticket = 0
+        if name is None:
+            name = uuid4().hex[:5] # get a (almost) unique name
+        self.name: str = name
+        if graph_record:
+            self.graph_record()
+            
+    def get_stencil_id(self):
+        stencil_id = self._stencil_id_ticket
+        self._stencil_id_ticket += 1
+        return stencil_id
+
+    def get_node_name(self, node_name: str, stencil_id: int):
+        return f'{node_name}_cluster_{stencil_id}'
+
+    def graph_record(self, filename: Optional[str] = None):
+        self._graph_record = True
+        if filename is None:
+            filename = f"{self.name}_graph.gv"
+        self._graph = Digraph(name=f"{self.name}_graph", filename=filename)
+
+    def graph_add_stencil(self, stencil: StencilObject, access_info: Dict[str, AccessKind], stencil_id: int):
+        args_name = ",".join(k for k in access_info)
+        stencil_name = f"{stencil.options['module']}_{stencil.options['name']}({args_name})"
+        row_ind, col_ind = self.get_kernel_dependencies(stencil)
+        num_kernels = len(row_ind) - 1
+        with self._graph.subgraph(name=f'cluster_{stencil_id}') as c:
+            c.attr(style='filled', color='lightgrey')
+            c.node_attr.update(style='filled', color='white')
+            start_name = self.get_node_name('start', stencil_id)
+            end_name = self.get_node_name('end', stencil_id)
+            c.node(start_name, label='start', shape='box')
+            c.node(end_name, label='end', shape='box')
+            c.attr(label=stencil_name)
+            for i in range(num_kernels):
+                cols = col_ind[row_ind[i]: row_ind[i+1]]
+                name_i = self.get_node_name(f'kernel{i}', stencil_id)
+                c.node(name_i, label=f"kernel {i}")
+                c.edge(start_name, name_i)
+                c.edge(name_i, end_name)
+                for j in cols:
+                    name_j = self.get_node_name(f'kernel{j}', stencil_id)
+                    c.edge(name_j, name_i)
+
+    def graph_stop_record(self):
+        self._graph_record = False
+
+    def graph_view(self, cleanup=True):
+        if isinstance(self._graph, Digraph):
+            self._graph.view(cleanup=cleanup)
+
+    def graph_save(self, cleanup=True):
+        if isinstance(self._graph, Digraph):
+            self._graph.render(cleanup=cleanup, format="pdf")
 
     def add_streams(self, num_streams):
         self.stream_pool.extend(cupy.cuda.Stream(non_blocking=True) for _ in range(num_streams))
@@ -115,7 +173,18 @@ class AsyncContext():
                 access_info[field_name] = access_kind
         return access_info
 
-    def get_dependencies(self, access_info: Dict[str, AccessKind]) -> List[cupy.cuda.Event]:
+    def get_kernel_dependencies(self, stencil: StencilObject) -> Tuple[List[int], List[int]]: #(row_ind, col_ind)
+        assert hasattr(stencil, "pyext_module")
+        num_kernels = stencil.pyext_module.num_kernels()
+        col_ind = []
+        row_ind = [0] * (num_kernels + 1)
+        if stencil.pyext_module.has_dependency_info():
+            row_ind = stencil.pyext_module.dependency_row_ind()
+            col_ind = stencil.pyext_module.dependency_col_ind()
+            assert len(row_ind) == num_kernels + 1, "CSR format in dependency data is broken"
+        return row_ind, col_ind
+
+    def get_dependencies(self, access_info: Dict[str, AccessKind], stencil_id: int) -> List[cupy.cuda.Event]:
         # R -> W, W -> W, W -> R
         dep_events = []
         reads = {k for k in access_info if access_info[k] == AccessKind.READ_ONLY}
@@ -125,27 +194,36 @@ class AsyncContext():
                 access_info_i = stencil_i.access_info
                 reads_i = {k for k in access_info_i if access_info_i[k] == AccessKind.READ_ONLY}
                 writes_i = {k for k in access_info_i if access_info_i[k] == AccessKind.READ_WRITE}
+                dep_flag = False
                 if writes.intersection(reads_i): # R -> W
-                    dep_events.append(stencil_i.done_event)
-                    continue
+                    dep_flag = True
                 if writes.intersection(writes_i): # W -> W
-                    dep_events.append(stencil_i.done_event)
-                    continue
+                    dep_flag = True
                 if reads.intersection(writes_i): # W -> R
+                    dep_flag = True
+                if dep_flag:
                     dep_events.append(stencil_i.done_event)
-                    continue
+                    if self._graph_record:
+                        node_name_i = self.get_node_name('end', stencil_i.id)
+                        node_name = self.get_node_name("start", stencil_id)
+                        self._graph.edge(node_name_i, node_name, style='bold', color='blue')
         return dep_events
 
-    def add_invoked_stencil(self, stencil: StencilObject, access_info: Dict[str, AccessKind], done_event: cupy.cuda.Event):
+    def add_invoked_stencil(self, stencil: StencilObject, access_info: Dict[str, AccessKind], done_event: cupy.cuda.Event, stencil_id: int):
         while len(self.invoked_stencils) >= self.max_invoked_stencils:
             sleep(self.sleep_time) # wait for gpu computation
             self.free_finished_stencils()
-        self.invoked_stencils.append(InvokedStencil(stencil=stencil, access_info=access_info, done_event=done_event))
+        self.invoked_stencils.append(InvokedStencil(stencil=stencil, access_info=access_info, done_event=done_event, id=stencil_id))
 
-    def wait_finish(self):
+    def wait(self):
         while len(self.invoked_stencils) > 0:
             sleep(self.sleep_time)
             self.free_finished_stencils()
+
+    def wait_finish(self):
+        self.wait()
+        for i in range(len(self.stream_pool)):
+            self.stream_pool[i] = None
 
     def schedule(self, stencil: StencilObject, *args, **kwargs):
         if self.blocking:
@@ -163,22 +241,32 @@ class AsyncContext():
         Step 5: invoke stencil
         Step 6: insert stop & wait events
         """
+        # remove finished calls
         self.free_finished_stencils()
-        access_info = self.get_field_access_info(stencil, *args, **kwargs)
-        dep_events = self.get_dependencies(access_info)
+
+        # check stencil obj is generated by the right backend
         has_kernel_dependency_info = False
-        if (hasattr(stencil, 'pyext_module')):
-            num_kernels = stencil.pyext_module.num_kernels()
-            has_kernel_dependency_info = stencil.pyext_module.has_dependency_info()
-        else:
-            raise TypeError(f"The stencil object {stencil.__module__}.{stencil.__name__} is not generated by GTC:CUDA backend")
+
+        assert hasattr(stencil, 'pyext_module'), f"The stencil object {stencil.__module__}.{stencil.__name__} is not generated by GTC:CUDA backend"
+        num_kernels = stencil.pyext_module.num_kernels()
+        has_kernel_dependency_info = stencil.pyext_module.has_dependency_info()
+
+        # resolve dependency
+        access_info = self.get_field_access_info(stencil, *args, **kwargs)
+        stencil_id = self.get_stencil_id()
+        if self._graph_record:
+            self.graph_add_stencil(stencil, access_info, stencil_id)
+        dep_events = self.get_dependencies(access_info, stencil_id)
+
         # count how many streams needed
         num_streams = num_kernels if has_kernel_dependency_info else 1  # TODO: reduce unnecessary streams
         stream_pool = self.allocate_streams(num_streams)
-        # insert events
+
+        # insert events for waiting dependencies
         for stream in stream_pool:
             for dep_event in dep_events:
                 stream.wait_event(dep_event)
+
         # Launch stencil
         if num_streams == 1:
             streams = stream_pool * num_kernels
@@ -186,14 +274,16 @@ class AsyncContext():
             streams = stream_pool
         stream_ptrs = [stream.ptr for stream in streams]
         stencil(*args, async_launch=True, streams=stream_ptrs, **kwargs)
-        # insert events
+
+        # insert events to record when the stencil finishes
         done_events = [cupy.cuda.Event(block=False, disable_timing=True) for _ in range(num_streams)]
         for i in range(1, num_streams):
             done_events[i].record(stream_pool[i])
             stream_pool[0].wait_event(done_events[i])
         done_events[0].record(stream_pool[0])
+
         # update async_ctx
-        self.add_invoked_stencil(stencil, access_info, done_events[0])
+        self.add_invoked_stencil(stencil, access_info, done_events[0], stencil_id)
 
 class InsertAsync(ast.NodeTransformer):
     @classmethod
